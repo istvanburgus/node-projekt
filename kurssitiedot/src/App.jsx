6d9f1990e1b465224e840ac188a7907e12d9a744
istvanburgus <burgus23@gmail.com>
2025-08-19 10:49:11 +0300

    1.5.1 propsien destrukturointi

--- kurssitiedot/src/App.jsx
+++ kurssitiedot/src/App.jsx
@@ -1,26 +1,24 @@
-function Header(props) {
-  return <h1>{props.course}</h1>
+function Header({ course }) {
+  return <h1>{course}</h1>
 }
 
-function Content(props) {
+function Content({ parts }) {
   return (
     <div>
-      <p>{props.parts[0].name} {props.parts[0].exercises}</p>
-      <p>{props.parts[1].name} {props.parts[1].exercises}</p>
-      <p>{props.parts[2].name} {props.parts[2].exercises}</p>
+      <p>{parts[0].name} {parts[0].exercises}</p>
+      <p>{parts[1].name} {parts[1].exercises}</p>
+      <p>{parts[2].name} {parts[2].exercises}</p>
     </div>
   )
 }
 
-function Total(props) {
-  // külön változó az összegnek
-  const sum = props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises
+function Total({ parts }) {
+  const sum = parts[0].exercises + parts[1].exercises + parts[2].exercises
   return <p>Number of exercises {sum}</p>
 }
 
 function App() {
   const course = 'Half Stack application development'
-
   const parts = [
     { name: 'Fundamentals of React', exercises: 10 },
     { name: 'Using props to pass data', exercises: 7 },
