0128eedc489e7ffcd8f09b9f3f038af492c461b7
istvanburgus <burgus23@gmail.com>
2025-11-06 09:41:40 +0200

    2.6 puhelinluettelo – lomakkeen käsittely

--- osa2/kurssitiedot/src/App.jsx
+++ osa2/kurssitiedot/src/App.jsx
@@ -1,35 +1,54 @@
-// 2.1–2.3 kurssitiedot: Course + map + reduce
-
-import Course from './components/Course'
+import { useState } from 'react'
 
 export default function App() {
-  const courses = [
-    {
-      id: 1,
-      name: 'Half Stack application development',
-      parts: [
-        { id: 1, name: 'Fundamentals of React', exercises: 10 },
-        { id: 2, name: 'Using props to pass data', exercises: 7 },
-        { id: 3, name: 'State of a component', exercises: 14 },
-        { id: 4, name: 'Redux', exercises: 11 },
-      ],
-    },
-    {
-      id: 2,
-      name: 'Node.js',
-      parts: [
-        { id: 1, name: 'Routing', exercises: 3 },
-        { id: 2, name: 'Middlewares', exercises: 7 },
-      ],
-    },
-  ]
+  const [persons, setPersons] = useState([
+    { id: 1, name: 'Arto Hellas' },
+    { id: 2, name: 'Ada Lovelace' }
+  ])
+
+
+  const [newName, setNewName] = useState('')
+
+  
+  const handleSubmit = (event) => {
+    event.preventDefault() 
+
+
+    const newPerson = {
+      id: persons.length ? Math.max(...persons.map(p => p.id)) + 1 : 1,
+      name: newName.trim()
+    }
+
+    if (!newPerson.name) return
+
+    setPersons(persons.concat(newPerson))
+    setNewName('')
+  }
 
   return (
     <div>
-      <h1>Web development curriculum</h1>
-      {courses.map(c => (
-        <Course key={c.id} course={c} />
-      ))}
+      <h1>Phonebook</h1>
+
+      <form onSubmit={handleSubmit}>
+        <div>
+          name:{' '}
+          <input
+            value={newName}
+            onChange={(e) => setNewName(e.target.value)}
+            placeholder="type a name"
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map(p => (
+          <li key={p.id}>{p.name}</li>
+        ))}
+      </ul>
     </div>
   )
 }
