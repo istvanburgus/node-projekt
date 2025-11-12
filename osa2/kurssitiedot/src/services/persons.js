6476284a2039712d05e9b1c500f78b171c1b9856
istvanburgus <burgus23@gmail.com>
2025-11-12 19:34:19 +0200

    2.15 kurssitiedot – persons.js lisätty (services-moduuli)

---
+++ osa2/kurssitiedot/src/services/persons.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+
+const baseUrl = 'http://localhost:3001/persons'
+
+// GET
+const getAll = () => {
+  return axios.get(baseUrl).then(res => res.data)
+}
+
+// POST
+const create = newPerson => {
+  return axios.post(baseUrl, newPerson).then(res => res.data)
+}
+
+// DELETE
+const remove = id => {
+  return axios.delete(`${baseUrl}/${id}`).then(res => res.data)
+}
+
+export default { getAll, create, remove }
