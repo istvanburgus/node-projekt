8edb61fbce17a637f4276f57f9b4d1a9c5a7ceac
istvanburgus <burgus23@gmail.com>
2025-12-01 12:36:19 +0200

    4.2.2: lisätty controllers/blogs ja siirretty routet

--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -2,7 +2,8 @@ const express = require('express')
 const mongoose = require('mongoose')
 const logger = require('./utils/logger')
 const config = require('./utils/config')
-const Blog = require('./models/blog')
+const blogsRouter = require('./controllers/blogs')
+
 
 const app = express()
 
@@ -17,20 +18,8 @@ mongoose
   })
 
 app.use(express.json())
+app.use('/api/blogs', blogsRouter)
 
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs)
-  })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog.save().then((result) => {
-    response.status(201).json(result)
-  })
-})
 
 app.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
