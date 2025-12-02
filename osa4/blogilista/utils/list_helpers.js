1ae4bdecc9f1d29132132709e7defb5a45250578
istvanburgus <burgus23@gmail.com>
2025-12-02 12:41:48 +0200

    Tehtävät 4.4 ja 4.5: totalLikes ja favouriteBlog testeineen

--- osa4/blogilista/utils/list_helpers.js
+++ osa4/blogilista/utils/list_helpers.js
@@ -1,7 +1,36 @@
+// 4.3 dummy-funktio – palauttaa aina arvon 1
 const dummy = (blogs) => {
   return 1
 }
 
+// 4.4 totalLikes – laskee blogien yhteenlasketut tykkäykset
+const totalLikes = (blogs) => {
+  return blogs.reduce((sum, blog) => {
+    return sum + (blog.likes || 0)
+  }, 0)
+}
+
+// 4.5 favouriteBlog – palauttaa eniten tykkäyksiä saaneen blogin
+const favouriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return null
+  }
+
+  let favourite = blogs[0]
+
+  blogs.forEach((blog) => {
+    if (blog.likes > favourite.likes) {
+      favourite = blog
+    }
+  })
+
+  // Jos usealla blogilla on sama määrä tykkäyksiä,
+  // palautetaan listassa ensimmäisenä oleva suosikki
+  return favourite
+}
+
 module.exports = {
   dummy,
+  totalLikes,
+  favouriteBlog,
 }
