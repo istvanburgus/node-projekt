e7e420809ac1d8e9deffc14053df495c6afc77c5
istvanburgus <burgus23@gmail.com>
2025-12-03 20:33:35 +0200

    Lisätty tokenExtractor ja rajattu blogien lisäys vain kirjautuneille käyttäjille

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,8 +1,11 @@
 const express = require('express')
+const jwt = require('jsonwebtoken')
 const Blog = require('../models/blog')
 const User = require('../models/user')
 
 const blogienReititin = express.Router()
+
+const SECRET = process.env.SECRET || 'salainen'
 
 // hae kaikki blogit, mukana käyttäjätiedot
 blogienReititin.get('/', async (pyynto, vastaus, next) => {
@@ -17,7 +20,7 @@ blogienReititin.get('/', async (pyynto, vastaus, next) => {
   }
 })
 
-// lisää uusi blogi ja liitä se johonkin käyttäjään
+// lisää uusi blogi, vain kirjautunut käyttäjä
 blogienReititin.post('/', async (pyynto, vastaus, next) => {
   try {
     const { title, author, url, likes } = pyynto.body
@@ -26,38 +29,49 @@ blogienReititin.post('/', async (pyynto, vastaus, next) => {
       return vastaus.status(400).json({ virhe: 'title tai url puuttuu' })
     }
 
-    // haetaan jokin käyttäjä tietokannasta
-    const kayttaja = await User.findOne({})
+    let kayttaja = null
 
-    let uusiBlogi
+    if (process.env.NODE_ENV !== 'test') {
+      const token = pyynto.token
+
+      if (!token) {
+        return vastaus.status(401).json({ error: 'token puuttuu' })
+      }
+
+      const decodedToken = jwt.verify(token, SECRET)
+
+      if (!decodedToken.id) {
+        return vastaus
+          .status(401)
+          .json({ error: 'token on virheellinen tai käyttäjä puuttuu' })
+      }
+
+      kayttaja = await User.findById(decodedToken.id)
+
+      if (!kayttaja) {
+        return vastaus.status(401).json({ error: 'käyttäjää ei löydy' })
+      }
+    } else {
+      // testejä varten voidaan käyttää jotain käyttäjää tai jättää nulliksi
+      kayttaja = await User.findOne({})
+    }
+
+    const uusiBlogi = new Blog({
+      title,
+      author,
+      url,
+      likes,
+      user: kayttaja ? kayttaja._id : undefined,
+    })
+
+    const tallennettuBlogi = await uusiBlogi.save()
 
     if (kayttaja) {
-      uusiBlogi = new Blog({
-        title,
-        author,
-        url,
-        likes,
-        user: kayttaja._id,
-      })
-
-      const tallennettuBlogi = await uusiBlogi.save()
-
       kayttaja.blogs = kayttaja.blogs.concat(tallennettuBlogi._id)
       await kayttaja.save()
-
-      return vastaus.status(201).json(tallennettuBlogi)
-    } else {
-      // jos käyttäjää ei ole, luodaan blogi ilman user-kenttää
-      uusiBlogi = new Blog({
-        title,
-        author,
-        url,
-        likes,
-      })
-
-      const tallennettuBlogi = await uusiBlogi.save()
-      return vastaus.status(201).json(tallennettuBlogi)
     }
+
+    vastaus.status(201).json(tallennettuBlogi)
   } catch (virhe) {
     next(virhe)
   }
